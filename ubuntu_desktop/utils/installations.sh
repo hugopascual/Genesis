7c2e8b074cd4e970426f43a67a81e014a18aa528
hugopascual <hugo.pascual.adan@gmail.com>
2024-03-29 12:30:40 +0100

    Added GitHub CLI install

--- ubuntu_desktop/utils/installations.sh
+++ ubuntu_desktop/utils/installations.sh
@@ -234,3 +234,19 @@ install_openvpnc() {
     echo_installed "OpenVPNC installed"
 }
 
+##
+# @Description
+# Install GitHub CLI
+# Github client to make login via terminal
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+##
+install_openvpnc() {
+    echo_info "Installing GitHub CLI"
+    sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+    echo_installed "GitHub CLI installed"
+}
+
