8cfbe0a5b7ba3239c37413ef579dac42698670f8
hugopascual <hugo.pascual.adan@gmail.com>
2024-07-18 22:10:17 +0200

    import and changes from ubuntu

--- debian_based_distros/ubuntu/test.sh
+++ debian_based_distros/ubuntu/test.sh
@@ -4,11 +4,11 @@
 # Get the path to the main directory.
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
 BASE_DIR="$(dirname "$FULL_PATH_TO_SCRIPT")"
-UTILS_DIR="$BASE_DIR/functions/"
+FUNCTIONS_DIR="$BASE_DIR/functions/"
 SCRIPTS_DIR="$BASE_DIR/scripts/"
 
 # Import utils
 # shellcheck disable=SC1090
-for f in $( find "$UTILS_DIR" -type f -print | sort ); do source "$f"; done
+for f in $( find "$FUNCTIONS_DIR" -type f -print | sort ); do source "$f"; done
 
 #------------------------------- Test
