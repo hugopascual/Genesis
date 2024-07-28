afd32aa496158aef67af30b5e46b87a622074afd
hugopascual <hugo.pascual.adan@gmail.com>
2024-07-28 17:48:51 +0200

    small fix

--- debian_based_distros/ubuntu/functions/installations_extra.sh
+++ debian_based_distros/ubuntu/functions/installations_extra.sh
@@ -10,10 +10,8 @@
 ##
 install_obsidian() {
     NAME="Obsidian"
-	echo_installing "$NAME"
     URL_DEB="https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.7/obsidian_1.6.7_amd64.deb"
     deb_download_and_install "Obsidian" "$URL_DEB"
-    echo_installed "$NAME"
 }
 
 ##
