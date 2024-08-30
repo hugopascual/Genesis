d4afee0e5b72ace5256ffcf76c593d0173f602e0
hugopascual <hugo.pascual.adan@gmail.com>
2024-08-30 20:12:56 +0200

    basic server setup script

---
+++ linux/server_setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+source "./select_distro.sh"
+
+#------------------------------- Start with the setup
+echo_info "Starting setup"
+
+#------------------------------- Update and Upgrade
+# shellcheck disable=SC1091
+source "$LINUX_SCRIPTS_PATH/update_system.sh"
+
+#------------------------------- Customization
+# shellcheck disable=SC1091
+source "$LINUX_SCRIPTS_PATH/locales_config.sh"
+
+#------------------------------- Software installations
+# Basic
+linux_install_net_basic
+linux_install_common_utilities
+linux_install_fastfetch
+linux_install_nvtop
+
+# Development
+linux_install_docker
+linux_install_github_cli
+
+# DIT Installs and config
+linux_install_openvpnc
+
+# Services
+
+# Games
+
+# Extra
+
+#------------------------------- Software configuration
+
+#------------------------------- Update and Clean
+# shellcheck disable=SC1091
+source "$LINUX_SCRIPTS_PATH/update_system.sh"
+
+#------------------------------- Finish
+echo_success "Restart the computer now"
