61fdfba3c35f2a639ea33ba6bf73dd953022e8e4
hugopascual <hugo.pascual.adan@gmail.com>
2024-09-22 23:43:45 +0200

    raspberryPi files typo and added fastfecth exception

---
+++ linux/distro_specific/debian_based/raspberryPi/raspberryPi_functions/raspberryPi_installations_basic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###############
+#--Utilities--#
+###############
+
+##
+# @Description
+# Install Fastfetch
+# Terminal tool used to show the characteristics of the machine in a pretty way 
+# https://github.com/fastfetch-cli/fastfetch
+##
+install_fastfetch() {
+    echo_info "Not installing fastfetch on RaspberryPi"
+}
