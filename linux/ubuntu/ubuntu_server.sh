49b70b66e302b5a97b82f451f591294766877fe2
hugopascual <hugo.pascual.adan@gmail.com>
2025-03-02 19:43:03 +0100

    ubuntu_server.sh first version

--- linux/ubuntu/ubuntu_server.sh
+++ linux/ubuntu/ubuntu_server.sh
@@ -2,4 +2,23 @@
 
 server_setup() {
     echo "server_setup"
+
+    #------------------------------- Customization
+
+    #------------------------------- Software installations
+    # Basic
+    ubuntu_install_net_basic
+    ubuntu_install_common_funcionalities
+    ubuntu_install_fastfetch
+    ubuntu_install_nvtop
+
+    # Development
+    ubuntu_install_docker
+    ubuntu_install_python
+
+    #------------------------------- Update and clean
+    update
+
+    #------------------------------- Finish
+    echo_success "Restart the computer now"
 }
