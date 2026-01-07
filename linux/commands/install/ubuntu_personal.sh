b4e7b5d9f326eaa1eaa6496f811cb931e2b164fc
hugopascual <hugo.pascual.adan@gmail.com>
2026-01-07 20:09:03 +0100

    ignore warning

--- linux/commands/install/ubuntu_personal.sh
+++ linux/commands/install/ubuntu_personal.sh
@@ -40,6 +40,7 @@ apt_get_install "nvtop"
 apt_get_install "git"
 # GitHub Cli
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+# shellcheck disable=SC2174
 mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 && chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
