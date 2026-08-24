668db309d68d346d3368357fee421eeb05e0a82c
hugopascual <hugo.pascual.adan@gmail.com>
2026-08-24 13:32:13 +0200

    feat: KDE main shortcuts

--- linux/src/setups/cachyos/kde.sh
+++ linux/src/setups/cachyos/kde.sh
@@ -39,17 +39,20 @@ kwriteconfig6 --file kcminputrc --group Touchpad --key NaturalScroll false
 
 # Keyboard shortcuts
 ## Change virtual desktops with Ctrl+Alt+Left/Right
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch One Desktop to the Left" "Ctrl+Alt+Left"
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch One Desktop to the Right" "Ctrl+Alt+Right"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch One Desktop to the Left" "Ctrl+Alt+Left,none,Switch One Desktop to the Left"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch One Desktop to the Right" "Ctrl+Alt+Right,none,Switch One Desktop to the Right"
 
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 1" "Meta+1"
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 2" "Meta+2"
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 3" "Meta+3"
-kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 4" "Meta+4"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch to Desktop 1" "Meta+1,none,Switch to Desktop 1"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch to Desktop 2" "Meta+2,none,Switch to Desktop 2"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch to Desktop 3" "Meta+3,none,Switch to Desktop 3"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Switch to Desktop 4" "Meta+4,none,Switch to Desktop 4"
+
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group kwin --key "Overview" "Meta,none,Toggle Overview"
+kwriteconfig6 --file "$HOME/.config/kglobalshortcutsrc" --group plasmashell --key "activate application launcher" "Meta+W,none,Activate Application Launcher"
 
 ################################################################
 # Add second keyboard distribution
-
+ 
 kwriteconfig6 --file kxkbrc --group Layout --key Use true
 kwriteconfig6 --file kxkbrc --group Layout --key LayoutList "us,es"
 kwriteconfig6 --file kxkbrc --group Layout --key Options "grp:win_space_toggle"
